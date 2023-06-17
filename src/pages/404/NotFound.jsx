1746da0821d80be74a67d8ea5f5151b2fb043c9a
toyesh0 <toyeshsaini567@gmail.com>
2023-06-17 13:59:19 +0530

    error fixed

--- src/pages/404/NotFound.jsx
+++ src/pages/404/NotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import './style.scss';
+import React from "react";
 
-const NotFound = () => {
-  return (
-    <div>404</div>
-  )
-}
+import "./style.scss";
 
-export default NotFound;
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
+
+export default PageNotFound;
